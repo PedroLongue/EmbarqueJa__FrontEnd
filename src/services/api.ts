d6c6b652c3b437e6d53431b19768e4fefe32ce8e
PedroLongue <pedrolongue13@gmail.com>
2025-06-19 12:15:06 -0300

    fix: adjust https

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: window.location.hostname.includes('localhost')
     ? 'http://localhost:3000/api'
-    : 'http://embarqueja.xyz/api',
+    : 'https://embarqueja.xyz/api',
 });
 
 export default api;
--- src/services/socket.ts
+++ src/services/socket.ts
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 const socket = io(
   window.location.hostname.includes('localhost')
     ? 'http://localhost:3000'
-    : 'http://embarqueja.xyz/',
+    : 'https://embarqueja.xyz/',
 );
 
 export default socket;
