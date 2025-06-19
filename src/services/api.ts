9166aa2f71d10fc4d59af5530b60bea1dcb56c8d
PedroLongue <pedrolongue13@gmail.com>
2025-06-19 10:01:12 -0300

    fix: adjust endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: window.location.hostname.includes('localhost')
     ? 'http://localhost:3000/api'
-    : 'http://31.97.171.60:3000/api',
+    : 'http://embarqueja.xyz/api',
 });
 
 export default api;
