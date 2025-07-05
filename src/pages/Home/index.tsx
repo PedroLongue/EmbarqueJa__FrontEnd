f044a397e55189bf780e63b4c24baf6c0043f407
Pedro Longue Correa <pedro.longue@hiplatform.com>
2025-07-05 16:59:33 -0300

    reafactor: change banner image

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { Box, Container } from '@mui/material';
-import Banner from '../../assets/imgs/banneHome2.png';
+import Banner from '../../assets/imgs/bannerHome.png';
 import RenderTickets from './components/RenderTickets';
 import SeachForm from './components/SearchForm';
 
